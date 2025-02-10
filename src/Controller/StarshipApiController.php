36f8b6cad57bfd66dbcdeb968b5d84dda2d3dacb
Irene <irene@example.com>
2025-02-10 18:28:05 +0100

    clone camilo

--- src/Controller/StarshipApiController.php
+++ src/Controller/StarshipApiController.php
@@ -5,19 +5,30 @@ namespace App\Controller;
 use App\Repository\StarshipRepository;
 use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+#[Route('/api/starships')]
 class StarshipApiController extends AbstractController
 {
-    #[Route('/api/starships/{id<\d+>}', methods:['GET'])]
-    public function getCollection(LoggerInterface $logger, StarshipRepository $repository, int $id): Response
+    #[Route('', methods:['GET'])]
+    public function getCollection(LoggerInterface $logger, StarshipRepository $repository): Response
     {
-        dd($id);
 
         $logger->info('Starship collection retrieved');
         $starships = $repository->findAll();
 
         return $this->json($starships);
+    }
+
+    #[Route('/{id<\d+>}', ['GET'])]
+    public function get(int $id, StarshipRepository $repository): JsonResponse
+    {
+        $starship = $repository->find($id);
+        if(!$starship){
+            throw $this->createNotFoundException('Starship not found');
+        }
+        return $this->json($starship);
     }
 }
